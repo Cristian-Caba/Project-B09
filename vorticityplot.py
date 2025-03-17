a374d308a44624fbe26c28cce34fd6818b46d649
Cristian Caba <cristiandimitriecaba@yahoo.com>
2025-03-17 11:59:54 +0100

    fixed plotting

--- vorticityplot.py
+++ vorticityplot.py
@@ -4,6 +4,15 @@ np.set_printoptions(threshold=np.inf)
 from pathlib import Path
 fig, axes = plt.subplots(4, 6, figsize=(12, 18))
 axes = axes.flatten()
+file_path=Path('Project-B09/Vorticity')/f'Case_SC_Span_{1}.txt_vorticity.csv'
+with open(file_path) as file2:
+    matrix=np.genfromtxt(file2, delimiter=',', filling_values=np.nan)
+x=matrix[0][1:]
+y=matrix[1:,0]
+z_sliced=matrix[1:,1:]
+vmin=np.min(z_sliced)
+vmax=np.max(z_sliced)
+
 for i in range (0,24):
     file_path=Path('Project-B09/Vorticity')/f'Case_SC_Span_{i+1}.txt_vorticity.csv'
     with open(file_path) as file2:
@@ -11,11 +20,12 @@ for i in range (0,24):
     x=matrix[0][1:]
     y=matrix[1:,0]
     z_sliced=matrix[1:,1:]
+    
     nrrow=len(matrix)
     nrcol=len(matrix[1])
     X, Y = np.meshgrid(x, y)
     Z=z_sliced
-    c = axes[i].contourf(X, Y, Z, levels=20, cmap='jet')
+    c = axes[i].contourf(X, Y, Z, levels=50, cmap='jet',vmin=vmin,vmax=vmax)
     axes[i].set_title(f"Frame{i+1}")
     axes[i].set_xlabel('Chord')
     axes[i].set_ylabel('Height')
