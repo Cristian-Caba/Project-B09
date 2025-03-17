a4dfa0ad7b0237e08ba4c5b1cb1c25db07b59249
Cristian Caba <cristiandimitriecaba@yahoo.com>
2025-03-17 12:24:53 +0100

    fixed subplots

--- vorticityplot.py
+++ vorticityplot.py
@@ -4,17 +4,24 @@ np.set_printoptions(threshold=np.inf)
 from pathlib import Path
 fig, axes = plt.subplots(4, 6, figsize=(12, 18))
 axes = axes.flatten()
-file_path=Path('Project-B09/Vorticity')/f'Case_SC_Span_{1}.txt_vorticity.csv'
-with open(file_path) as file2:
-    matrix=np.genfromtxt(file2, delimiter=',', filling_values=np.nan)
-x=matrix[0][1:]
-y=matrix[1:,0]
-z_sliced=matrix[1:,1:]
-vmin=np.min(z_sliced)
-vmax=np.max(z_sliced)
+ccsc="CC"
+
+vmin=0
+vmax=0
+for i in range (0,24):
+    file_path=Path('Project-B09/Vorticity')/f'Case_{ccsc}_Span_{i+1}.txt_vorticity.csv'
+    with open(file_path) as file2:
+        matrix=np.genfromtxt(file2, delimiter=',', filling_values=np.nan)
+    z_sliced=matrix[1:,1:]
+    Z=z_sliced
+    if vmin>np.min(Z):
+        vmin=np.min(Z)
+    if vmax<np.max(Z):
+        vmax=np.max(Z)    
+    
 
 for i in range (0,24):
-    file_path=Path('Project-B09/Vorticity')/f'Case_SC_Span_{i+1}.txt_vorticity.csv'
+    file_path=Path('Project-B09/Vorticity')/f'Case_{ccsc}_Span_{i+1}.txt_vorticity.csv'
     with open(file_path) as file2:
         matrix=np.genfromtxt(file2, delimiter=',', filling_values=np.nan)
     x=matrix[0][1:]
