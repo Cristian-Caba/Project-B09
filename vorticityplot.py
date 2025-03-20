b0588ebd8403d4563246103b526e73ed076fab90
Cristian Caba <cristiandimitriecaba@yahoo.com>
2025-03-20 11:54:26 +0100

    dimensionalizing

--- vorticityplot.py
+++ vorticityplot.py
@@ -4,7 +4,7 @@ np.set_printoptions(threshold=np.inf)
 from pathlib import Path
 fig, axes = plt.subplots(4, 6, figsize=(12, 18))
 axes = axes.flatten()
-ccsc="CC"
+ccsc="SC"
 
 vmin=0
 vmax=0
@@ -13,24 +13,24 @@ for i in range (0,24):
     with open(file_path) as file2:
         matrix=np.genfromtxt(file2, delimiter=',', filling_values=np.nan)
     z_sliced=matrix[1:,1:]
-    Z=z_sliced
-    if vmin>np.min(Z):
+    Z=z_sliced   
+if vmin>np.min(Z):
         vmin=np.min(Z)
-    if vmax<np.max(Z):
-        vmax=np.max(Z)    
-    
+if vmax<np.max(Z):
+        vmax=np.max(Z)     
 
 for i in range (0,24):
     file_path=Path('Project-B09/Vorticity')/f'Case_{ccsc}_Span_{i+1}.txt_vorticity.csv'
     with open(file_path) as file2:
         matrix=np.genfromtxt(file2, delimiter=',', filling_values=np.nan)
     x=matrix[0][1:]
-    y=matrix[1:,0]
-    z_sliced=matrix[1:,1:]
-    
+    y=matrix[20:,0]
+    z_sliced=matrix[20:,1:]
+    with open(f'Project-B09/PIV_planes_dimensionalised/Case_SC_Span_1.txt_u.csv') as file3:
+        xdimension=np.genfromtxt(file3, delimiter=',', filling_values=np.nan)[0][1:0]
     nrrow=len(matrix)
     nrcol=len(matrix[1])
-    X, Y = np.meshgrid(x, y)
+    X, Y = np.meshgrid(xdimension, y)
     Z=z_sliced
     c = axes[i].contourf(X, Y, Z, levels=50, cmap='jet',vmin=vmin,vmax=vmax)
     axes[i].set_title(f"Frame{i+1}")
